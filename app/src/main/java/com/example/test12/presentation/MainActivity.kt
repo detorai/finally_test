492a25a14cc2332f27551096557c16164bc548b3
detorai <detorai@>
2025-02-05 13:27:41 +0300

    FIX

--- app/src/main/java/com/example/test12/presentation/MainActivity.kt
+++ app/src/main/java/com/example/test12/presentation/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.navigator.Navigator
+import com.example.test12.presentation.sign_in.SignInScreen
 import com.example.test12.presentation.ui.theme.Test12Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Test12Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigator(SignInScreen())
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Test12Theme {
-        Greeting("Android")
-    }
-}
--- app/src/test/java/com/example/test12/ExampleUnitTest.kt
+++ app/src/test/java/com/example/test12/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.test12
 
+import com.example.test12.presentation.sign_in.SignInViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,8 +11,25 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    val viewModel = SignInViewModel()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun checkEmailEmpty(){
+        assert(
+        viewModel.checkEmailEmpty("123@123.com")
+        )
+    }
+    @Test
+    fun checkPasswordEmpty(){
+        assert(
+        viewModel.checkPasswordEmpty("123")
+        )
+    }
+    @Test
+    fun checkEmailPattern(){
+        assert(
+        viewModel.checkEmailPattern("123@123.com")
+        )
     }
 }
